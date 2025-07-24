ab3bf465f3362282b719daba9ed669325e8d990a
Maria <100872877+Maria00002@users.noreply.github.com>
2025-07-24 14:46:17 -0700

    Contain functions for basic math operators
    add, subtract, multiply, divide

---
+++ javascript.js
@@ -0,0 +1,26 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+
+console.log(add(13,16)); //29
+console.log(subtract(13,16)); //-3
+console.log(multiply(13,16)); //208
+console.log(divide(13,16)); //0.8125
+
+// console.log();
+
+
+
