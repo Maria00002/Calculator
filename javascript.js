8b99b59b1ebeb2a34f0a6825dbaf87eaf946cf69
Maria <100872877+Maria00002@users.noreply.github.com>
2025-07-25 16:49:12 -0700

    Create the layout for the calculator

--- javascript.js
+++ javascript.js
@@ -1,3 +1,29 @@
+let firstNum;
+let operator = "";
+let secondNum;
+
+function operate(firstNum, operator, secondNum) {
+    let result;
+    switch(operator) {
+        case "+":
+            result = add(firstNum, secondNum);
+            break;
+        case "-":
+            result = subtract(firstNum, secondNum);
+            break;
+        case "*":
+            result = multiply(firstNum, secondNum);
+            break;
+        case "/":
+            result = divide(firstNum, secondNum);
+            break;
+
+    }
+
+    return result;
+
+}
+
 function add(a, b) {
     return a + b;
 }
@@ -15,10 +41,16 @@ function divide(a, b) {
 }
 
 
-console.log(add(13,16)); //29
-console.log(subtract(13,16)); //-3
-console.log(multiply(13,16)); //208
-console.log(divide(13,16)); //0.8125
+console.log(operate(13, "+", 16));
+console.log(operate(13, "-", 16));
+console.log(operate(13, "*", 16));
+console.log(operate(13, "/", 16));
+
+
+// console.log(add(13,16)); //29
+// console.log(subtract(13,16)); //-3
+// console.log(multiply(13,16)); //208
+// console.log(divide(13,16)); //0.8125
 
 // console.log();
 
