394dad538918a92236f2574f44c99e281c1ad994
Maria <100872877+Maria00002@users.noreply.github.com>
2025-07-28 10:26:13 -0700

    Refactor the switch statement for button.id
    Refactor to make the code more readable by changing
    the output case conditions using function
    inputDigitStringConvertToDigit

--- javascript.js
+++ javascript.js
@@ -36,44 +36,31 @@ const bottom = document.querySelector("#bottom");
 const topDisplay = document.querySelector("#top");
 console.log(buttons); //
 
+function inputDigitStringConvertToDigit(digitString) {
+    let arrayDigit = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
+    let arrayDigitString = ["zero", "one","two" , "three","four" ,"five" ,"six" , "seven" ,"eight" , "nine"];    
+    return arrayDigit[arrayDigitString.indexOf(digitString)];
+}
+
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
         switch(button.id) {
             case "zero":
-                output += "0";
-                break;
             case "one":
-                output += "1";
-                break;
             case "two":
-                output += "2";
-                break;
             case "three":
-                output += "3";
-                break;
             case "four":
-                output += "4";
-                break;
             case "five":
-                output += "5";
-                break;
             case "six":
-                output += "6";
-                break;
             case "seven":
-                output += "7";
-                break;
             case "eight":
-                output += "8";
-                break;
             case "nine":
-                output += "9";
+                output += inputDigitStringConvertToDigit(button.id);
                 break;
             case "plus":
                 if (output.length > 0 && operator.length == 0) {
                     firstNum = parseInt(output);
                     output += ' + ';
-                    // console.log(firstNum);
                     operator = "plus";
                 } else if (operator.length > 0) {
                     if (output.split("+").length == 2) {
